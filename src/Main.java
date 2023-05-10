f01ec7e5cb246046cbf7a5af89bfcc4cddb0409c
Professional <ustu1982@mail.ru>
2023-05-10 19:31:35 +0500

    int

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    {
+        int[] taskOne = new int[3];
+        taskOne[0]= 1;
+        taskOne[1]= 2;
+        taskOne[2]= 3;
+    }
+
+}
