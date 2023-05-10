7463d4a6ca8e406648fbd336c49cf446d298b9e0
Professional <ustu1982@mail.ru>
2023-05-10 21:27:55 +0500

    three

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,38 @@ public class Main {
         one[1] = 2;
         one[2] = 3;
         for (int i = 0; i < one.length; i++) {
-            System.out.println(one[i]);
+            System.out.print(one[i] + " ");
         }
+        System.out.println();
         double[] two = {1.57, 7.654, 9.986};
         for (int i = 0; i < two.length; i++) {
-            System.out.println(two[i]);
+            System.out.print(two[i] + " ");
         }
+        System.out.println();
         int[] three = {1, 5, 89, 12, 25, 34};
         for (int i = 0; i < three.length; i++) {
-            System.out.println(three[i]);
+            System.out.print(three[i] + " ");
+        }
+        System.out.println();
+        for (int i = one.length - 1; i >= 0; i--) {
+            System.out.print(one[i]);
+            if (i != 0) {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+        for (int i = two.length - 1; i >= 0; i--) {
+            System.out.print(two[i]);
+            if (i != 0) {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+        for (int i = three.length - 1; i >= 0; i--) {
+            System.out.print(three[i]);
+            if (i != 0) {
+                System.out.print(" ");
+            }
         }
     }
 }
