f3ab54ca099f0dc6a4693b8bab012a99b3c3b07c
Professional <ustu1982@mail.ru>
2023-05-10 20:59:56 +0500

    two

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-    {
-        int[] taskOne = new int[3];
-        taskOne[0]= 1;
-        taskOne[1]= 2;
-        taskOne[2]= 3;
-    }
 
+        System.out.println("Hello world!");
+        int[] one = new int[3];
+        one[0] = 1;
+        one[1] = 2;
+        one[2] = 3;
+        for (int i = 0; i < one.length; i++) {
+            System.out.println(one[i]);
+        }
+        double[] two = {1.57, 7.654, 9.986};
+        for (int i = 0; i < two.length; i++) {
+            System.out.println(two[i]);
+        }
+        int[] three = {1, 5, 89, 12, 25, 34};
+        for (int i = 0; i < three.length; i++) {
+            System.out.println(three[i]);
+        }
+    }
 }
